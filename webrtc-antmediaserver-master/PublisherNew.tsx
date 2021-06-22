1862addd5b4dfc1c89775cd84399f1249679b0b2
rehamkhawajahere <rehamkhawaja@hotmail.com>
2021-06-22 17:10:08 +0200

    Publish start and stop

--- webrtc-antmediaserver-master/PublisherNew.tsx
+++ webrtc-antmediaserver-master/PublisherNew.tsx
@@ -22,115 +22,73 @@ export const PublisherNew = () => {
     bandwidth: 300,
     callback(command, data) {
       switch (command) {
-        case "pong":
-          break;
-        case "joinedTheRoom":
-        //   if ("onJoinedRoom" in events) {
-        //     const tok = data.ATTR_ROOM_NAME;
-        //     this.initPeerConnection(data.streamId, "publish");
-        //     this.publish(data.streamId, tok);
-
-        //     const streams = data.streams;
-
-        //     // if (streams != null) {
-        //     //   streams.forEach((item) => {
-        //     //     if (item === stream.id) return;
-        //     //     this.play(item, tok, roomId);
-        //     //   });
-        //     // }
-        //   }
-        //   break;
-
         case "publish":
           console.log("publish");
           this.initPeerConnection(data.streamId, "publish");
           this.publish(data.streamId);
 
           break;
-        case "streamJoined":
-          //   if ("onStreamJoined" in events) {
-          //     this.play(data.streamId, token, roomId);
-          //   }
+        case "stop":
+          console.log("stop adapter");
+
+          this.stop(data.streamId);
+
           break;
+
         default:
           break;
       }
     },
     callbackError: (err, data) => {
-      console.error("callbackError", err, data);
+      //   console.error("callbackError", err, data);
     },
   });
 
   const handleConnect = useCallback(() => {
     if (adaptor) {
-      const streamId = "Reham";
-      //   const roomId = "5abcd1";
+      console.log("publish");
+      adaptor.publish("reham");
+    }
+  }, [adaptor]);
 
-      stream.id = streamId;
-
-      adaptor.publish(streamId);
+  const closeConnect = useCallback(() => {
+    if (adaptor) {
+      console.log("close");
+      adaptor.stop("reham");
     }
   }, [adaptor]);
 
   useEffect(() => {
     if (adaptor) {
-      const verify = () => {
-        if (
-          adaptor.localStream.current &&
-          adaptor.localStream.current.toURL()
-        ) {
-          return setLocalStream(adaptor.localStream.current.toURL());
-        }
-        setTimeout(verify, 3000);
-      };
-      verify();
+      if (adaptor.localStream.current && adaptor.localStream.current.toURL()) {
+        setLocalStream(adaptor.localStream.current.toURL());
+        // handleConnect();
+      }
     }
   }, [adaptor]);
 
-  //   useEffect(() => {
-  //     if (adaptor && Object.keys(adaptor.remoteStreams).length > 0) {
-  //       for (let i in adaptor.remoteStreams) {
-  //         if (i !== stream.id) {
-  //           let st =
-  //             adaptor.remoteStreams[i][0] &&
-  //             "toURL" in adaptor.remoteStreams[i][0]
-  //               ? adaptor.remoteStreams[i][0].toURL()
-  //               : null;
-  //           setRemoteStream(st);
-  //           break;
-  //         } else {
-  //           setRemoteStream(null);
-  //         }
-  //       }
-  //     }
-  //   }, [adaptor, stream.id]);
-
   return (
     <SafeAreaView style={{ flex: 1 }}>
-      {localStream ? (
+      {!!localStream && (
         <RTCView
           style={{
-            width: 200,
-            height: 200,
-            position: "absolute",
-            bottom: 0,
-            right: 0,
+            flex: 1,
           }}
           objectFit="cover"
           streamURL={localStream}
-        />
-      ) : (
-        <Button
-          onPress={handleConnect}
-          title="Join room"
-          color="#841584"
-          accessibilityLabel="Connect to antmedia"
         />
       )}
 
       <Button
         onPress={handleConnect}
-        title="Join room"
+        title="Start"
+        color="#841584"
+        accessibilityLabel="Connect to antmedia"
+      />
+
+      <Button
+        onPress={closeConnect}
+        title="Stop"
         color="#841584"
         accessibilityLabel="Connect to antmedia"
       />
